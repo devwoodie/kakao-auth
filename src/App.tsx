0c556adab454995b8da92fbfda0cb34b30e52c55
devwoodie <ehddl453@naver.com>
2023-05-17 13:02:50 +0900

    feat: 카카오톡 회원가입 기능 구현 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import './App.css';
+import './reset.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import KakaoLogIn from "./KakaoLogIn";
+import KakaoAuthHandler from "./KakaoAuthHandler";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/"></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h2 className="title">Kakao Auth</h2>
+            <h5 className="sub-title">카카오톡 로그인 구현하기</h5>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<KakaoLogIn />}></Route>
+                    <Route path="/oauth" element={<KakaoAuthHandler />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/KakaoLogIn.tsx
+++ src/KakaoLogIn.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import KakaoBtn from "./kakao_login_medium_narrow.png";
 
 const KakaoLogIn = () => {
 
+    const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+    const REDIRECT_URI: string = "http://localhost:3000/oauth";
+    const KAKAO_AUTH_URL: string = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
+    const kakaoLogin = () => {
+        window.location.href = KAKAO_AUTH_URL;
+    }
 
     return(
         <>
-
+            <button onClick={kakaoLogin}><img src={KakaoBtn} alt="kakao_btn" /></button>
         </>
     )
 }
