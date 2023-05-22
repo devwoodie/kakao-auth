1c216011c32802f24f686f3220d4abefe701856e
devwoodie <ehddl453@naver.com>
2023-05-23 00:13:29 +0900

    feat: Access Token 발급과 사용자 정보 가져오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import './App.css';
 import './reset.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import KakaoLogIn from "./components/KakaoLogIn";
-import KakaoAuthHandler from "./components/KakaoAuthHandler";
 import Complete from "./components/Complete";
+import KakaoBtn from "./kakao_login_medium_narrow.png";
 
 function App() {
+
+    const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+    const REDIRECT_URI: string = "http://localhost:3000/oauth";
+    const KAKAO_AUTH_URL: string = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
     return (
         <div className="App">
             <h2 className="title">Kakao Auth</h2>
             <h5 className="sub-title">카카오톡 로그인 구현하기</h5>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<KakaoLogIn />}></Route>
-                    <Route path="/oauth" element={<KakaoAuthHandler />}></Route>
+                    <Route path="/" element={<Link className="btn" to={KAKAO_AUTH_URL}><img src={KakaoBtn} alt="kakao_btn" /></Link>} ></Route>
+                    <Route path="/oauth" element={<KakaoLogIn />}></Route>
                     <Route path="/comp" element={<Complete />}></Route>
                 </Routes>
             </BrowserRouter>
--- src/components/KakaoLogIn.tsx
+++ src/components/KakaoLogIn.tsx
@@ -1,22 +1,48 @@
-import React from "react";
-import KakaoBtn from "../kakao_login_medium_narrow.png";
+import React, {useEffect} from "react";
+import qs from "qs";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const KakaoLogIn = () => {
 
+    const navigate = useNavigate();
+
     const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-    console.log(REST_API_KEY)
     const REDIRECT_URI: string = "http://localhost:3000/oauth";
-    const KAKAO_AUTH_URL: string = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
-    const kakaoLogin = () => {
-        window.location.href = KAKAO_AUTH_URL;
-    }
+    const code = new URL(window.location.href).searchParams.get("code");
 
-    return(
-        <>
-            <button onClick={kakaoLogin}><img src={KakaoBtn} alt="kakao_btn" /></button>
-        </>
-    )
+    const getToken = async (): Promise<void> => {
+        const payload = qs.stringify({
+            grant_type: "authorization_code",
+            client_id: REST_API_KEY,
+            redirect_uri: REDIRECT_URI,
+            code: code,
+            client_secret: CLIENT_SECRET,
+        });
+
+        try{
+            const res = await axios.post(
+                "https://kauth.kakao.com/oauth/token",
+                payload,
+
+            )
+            console.log(res)
+            // window.Kakao.init(REST_API_KEY);
+            // access token 설정
+            // window.Kakao.Auth.setAccessToken(res.data.access_token);
+            navigate("/comp")
+        }catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getToken();
+    }, []);
+
+    return null;
 }
 
 export default KakaoLogIn;
